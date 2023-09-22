096563a71c1ab2bf3dc897ed0932fc882de06913
NULL <ORCx41@gmail.com>
2023-09-22 07:20:20 +0300

    .

--- Loader/HellsAsm.asm
+++ Loader/HellsAsm.asm
@@ -1,17 +1,17 @@
 .data
-	wSystemCall			DWORD	0h	
+	wSystemCall		DWORD	0h	
 	qSyscallInsAdress	QWORD	0h	
 
 
 .code
 
 	SetSSn proc	
-		xor eax, eax					; eax = 0
+		xor eax, eax				; eax = 0
 		mov wSystemCall, eax			; wSystemCall = 0
 		mov qSyscallInsAdress, rax		; qSyscallInsAdress = 0
-		mov eax, ecx					; eax = ssn
+		mov eax, ecx				; eax = ssn
 		mov wSystemCall, eax			; wSystemCall = eax = ssn
-		mov r8, rdx						; r8 = AddressOfASyscallInst
+		mov r8, rdx				; r8 = AddressOfASyscallInst
 		mov qSyscallInsAdress, r8		; qSyscallInsAdress = r8 = AddressOfASyscallInst
 		ret
 	SetSSn endp
@@ -28,18 +28,18 @@
 
 
 	RunSyscall proc
-		xor r10, r10						; r10 = 0
-		mov rax, rcx						; rax = rcx
-		mov r10, rax						; r10 = rax	= rcx
+		xor r10, r10					; r10 = 0
+		mov rax, rcx					; rax = rcx
+		mov r10, rax					; r10 = rax = rcx
 		mov eax, wSystemCall				; eax = ssn
-		jmp Run								; execute 'Run'
-		xor eax, eax	; wont run
-		xor rcx, rcx	; wont run
-		shl r10, 2		; wont run
+		jmp Run						; execute 'Run'
+		xor eax, eax					; wont run
+		xor rcx, rcx					; wont run
+		shl r10, 2					; wont run
 	Run:
 		jmp qword ptr [qSyscallInsAdress]
 		xor r10, r10					; r10 = 0
-		mov qSyscallInsAdress, r10		; qSyscallInsAdress = 0
+		mov qSyscallInsAdress, r10			; qSyscallInsAdress = 0
 		ret
 	RunSyscall endp
 
